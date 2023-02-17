7faaa997d36173521cc2aecf036464501781a1bb
ruxne <11211005@student.itk.ac.id>
2023-02-17 11:04:29 +0800

    fix color drawLine broken

--- src/utils/drawLine.ts
+++ src/utils/drawLine.ts
@@ -11,10 +11,12 @@ export default function drawLine(props: Props) {
     const { ctx, start, end, color,text } = props;
     ctx.beginPath();
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+    ctx.lineWidth = 1;
     ctx.moveTo(start.x, start.y);
     ctx.lineTo(end.x, -end.y);
     if (text) {
-        ctx.fillText(text, end.x, -end.y - 10);
+        ctx.fillText(text, end.x, -end.y - (end.y > 0 ? 10 : -10));
         ctx.textAlign = 'center';
         ctx.textBaseline = 'middle';
     }
