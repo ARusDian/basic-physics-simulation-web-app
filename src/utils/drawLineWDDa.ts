181a765e7ba82ac4324e82261cdb893bc347a680
ruxne <11211005@student.itk.ac.id>
2023-02-17 22:29:13 +0800

    fix : add infinity end point option

--- src/utils/drawLineWDDa.ts
+++ src/utils/drawLineWDDa.ts
@@ -1,18 +1,58 @@
 import DDA from "./DDA";
-import { Props } from "./drawLine";
 
-const drawLineDDA = ({ ctx, start, end, color, text } : Props) => {
-	// const values = DDA(start, end);
+interface Props {
+	ctx: CanvasRenderingContext2D;
+	start: { x: number, y: number };
+	end: { x: number, y: number };
+	color: string;
+	text?: string;
+	isInfinity?: boolean;
+	canvasHeight?: number;
+}
+
+const drawLineDDA = (props: Props) => {
+
+	const { ctx, start, end, color, text, isInfinity, canvasHeight } = props;
+	
 	ctx.beginPath();
 	ctx.strokeStyle = color;
-	ctx.moveTo(start.x, -start.y);
-	ctx.lineTo(end.x, -end.y);
-	// ctx.moveTo(values[0].x, -values[0].y);
-	// for (let i = 1; i < values.length; i+=10) {
-	// 	ctx.lineTo(values[i].x, -values[i].y);
-	// }
-	// ctx.lineTo(values[values.length-1].x, -values[values.length-1].y);
-	// ctx.lineTo(end.x, -end.y);
+	ctx.fillStyle = color;
+	ctx.lineWidth = 1;
+
+	if (isInfinity && canvasHeight && end) {
+
+		const slope = (end.y - start.y) / (end.x - start.x);
+		const intercept = start.y - slope * start.x;
+
+		const getX = (y: number) => {
+			return (y - intercept) / slope;
+		}
+
+		console.log(start, { x: getX(canvasHeight), y: - canvasHeight });
+
+
+		let { values, steps } = DDA(start, { x: getX(canvasHeight), y: - canvasHeight });
+
+		console.log(steps)
+	
+		ctx.moveTo(values[0].x, -values[0].y);
+		for (let i = 1; i < values.length; i += 10) {
+			ctx.lineTo(values[i].x, -values[i].y);
+		}
+		ctx.lineTo(values[values.length - 1].x, -values[values.length - 1].y);
+		ctx.lineTo(end.x, -end.y);
+	} else {
+		const { values, steps } = DDA(start, end);
+		ctx.moveTo(values[0].x, -values[0].y);
+		for (let i = 1; i < values.length; i += 10) {
+			ctx.lineTo(values[i].x, -values[i].y);
+		}
+		ctx.lineTo(values[values.length - 1].x, -values[values.length - 1].y);
+		ctx.lineTo(end.x, -end.y);
+	}
+
+
+	
 	if (text) {
 		ctx.fillText(text, end.x, -end.y - 10);
 		ctx.textAlign = 'center';
@@ -21,5 +61,5 @@ const drawLineDDA = ({ ctx, start, end, color, text } : Props) => {
 	ctx.stroke();
 	ctx.closePath();
 };
- 
+
 export default drawLineDDA;
