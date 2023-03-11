438cb7e873e9a42768f1ed68693f2575c9ba018c
AriqH <11211018@student.itk.ac.id>
2023-03-12 03:54:32 +0800

    implements dda and draw lens and mirror

---
+++ src/utils/Vector2f.ts
@@ -0,0 +1,27 @@
+class Vector2f {
+    private x: number;
+    private y: number;
+
+    public constructor(x?: number, y?: number) {
+        this.x = x ?? 0;
+        this.y = y ?? 0;
+    }
+
+    public getX(): number {
+        return this.x
+    }
+
+    public getY(): number {
+        return this.y
+    }
+
+    public setX(x: number): void {
+        this.x = x
+    }
+
+    public setY(y: number): void {
+        this.y = y
+    }
+}
+
+export default Vector2f;
