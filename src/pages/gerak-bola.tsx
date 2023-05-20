550e43b0012b7f254caa975c57ddfb6169607257
dimpram101 <dimas2004211@gmail.com>
2023-05-20 23:09:00 +0800

    feat: add gravity toggle

--- src/pages/gerak-bola.tsx
+++ src/pages/gerak-bola.tsx
@@ -243,7 +243,7 @@ export default function GerakBola() {
 				</div>
 				<div className="mt-3">
 					<label className="relative inline-flex items-center cursor-pointer">
-						<input type="checkbox" className="sr-only peer" checked={gravity ? true : false} onClick={() => setGravity(prev => !prev)} />
+						<input type="checkbox" className="sr-only peer" checked={gravity ? true : false} onChange={() => setGravity(prev => !prev)} />
 						<div className="w-11 h-6 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-800 rounded-full peer bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all border-gray-600 peer-checked:bg-blue-600"></div>
 						<span className="ml-3 text-lg font-medium text-gray-300">Gravity</span>
 					</label>
